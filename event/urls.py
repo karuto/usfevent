e33016f1f1da62dede6edca37b314f609a0d58c8
Vincent Zhang <iamkaruto@gmail.com>
2013-03-22 01:25:50 -0700

    Removed hardcoded URL structure in templates

--- event/urls.py
+++ event/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import ListView, DetailView
 from event.models import Event
+from event import views
 
 urlpatterns = patterns('event.views',
 
@@ -10,12 +11,13 @@ urlpatterns = patterns('event.views',
 
     url(r'^(?P<pk>\d+)$', DetailView.as_view(
 	model = Event,
-	template_name = "event_single.html")),
+	template_name = "event_single.html"),
+	name = 'single'),
 
     url(r'^archives/$', ListView.as_view(
 	queryset = Event.objects.all().order_by("-created"),
 	template_name = "event_listview.html")),
 
-    url(r'^tag/(?P<tag>\w+)$', 'tagpage'),
+    url(r'^tag/(?P<tag>\w+)$', 'tagpage', name = 'tag'),
 
 )
