7c83c14d482e2470ab51d7f0c044ca052387986d
Vincent Zhang <iamkaruto@gmail.com>
2013-03-22 00:44:13 -0700

    Added relative path for media

--- usfevent/settings.py
+++ usfevent/settings.py
@@ -54,7 +54,7 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
 # HARDCODED
-MEDIA_ROOT = '/home/vincent/Code/usfevent/media'
+MEDIA_ROOT = os.path.join(DIRNAME, '../media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
