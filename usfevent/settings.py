ffae561fb3b60dcb850626939eea4ec9cd7fbbdf
Vincent Zhang <iamkaruto@gmail.com>
2013-03-21 21:14:57 -0700

    Added media structure, minor change

--- usfevent/settings.py
+++ usfevent/settings.py
@@ -50,7 +50,8 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+# HARDCODED
+MEDIA_ROOT = '/home/vincent/Code/usfevent/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -72,6 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    # HARDCODED
     "/home/vincent/Code/usfevent/static",
 )
 
