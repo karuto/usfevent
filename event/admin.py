53f099566c0a2d4ea31a1695973244c6434197c5
Vincent Zhang <iamkaruto@gmail.com>
2013-04-04 19:01:05 -0700

    Downgraded to django 1.3, restarted whole project

--- event/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from event.models import Event
-
-admin.site.register(Event)
--- event/views.py
+++
@@ -1,7 +0,0 @@
-from event.models import Event
-from django.shortcuts import render_to_response
-
-# Create your views here.
-def tagpage(request, tag):
-	events = Event.objects.filter(tags__name = tag)
-	return render_to_response("tag_single.html", {"events":events, "tag":tag})
