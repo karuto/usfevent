ce70873de4ab3fa650bbef6e7269401ff74878b3
Vincent Zhang <iamkaruto@gmail.com>
2013-04-07 19:28:28 -0700

    Debugging require_debug_false

--- settings.py
+++ settings.py
@@ -92,7 +92,7 @@ SECRET_KEY = '#90glw&ozax8f=r2)x6hlg96r(g$!78kt_irn59b5$v0hipp=9'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -112,6 +112,11 @@ TEMPLATE_DIRS = (
     os.path.abspath("templates")
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (  
+    "django.core.context_processors.auth", 
+    "django.core.context_processors.request"  
+) 
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,26 +132,3 @@ INSTALLED_APPS = (
     'event',
     'accounts',
 )
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
