2e9dc7c53119b7d26bf8ff91730fabca3196a3ec
Vincent Zhang <iamkaruto@gmail.com>
2013-04-07 17:14:09 -0700

    Added event template

--- event/views.py
+++ event/views.py
@@ -1,12 +1,17 @@
 # Create your views here.
 from event.models import Event
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.shortcuts import render_to_response
+from django.template import Context, loader, RequestContext
 
 def index(request):
-	event_list = Event.objects.all().order_by('id')
-	
-	output = ', '.join([e.title for e in event_list])
-	return HttpResponse(output)
+    template_var = {}
+    try:
+        event_list = Event.objects.all().order_by('id')
+    except Event.DoesNotExist:
+        raise Http404
+    output = ', '.join([e.title for e in event_list])    
+    template_var["events"] = output	
+    return render_to_response("event/index.html", template_var)
 
 
