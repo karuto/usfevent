102ceb55da57d1d93f876ba7ba7694280067a2c8
Vincent Zhang <iamkaruto@gmail.com>
2013-04-10 22:10:55 -0700

    Changed name slightly, added South

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfile(models.Model):
-    userReference = models.ForeignKey(User)
+    django_user = models.ForeignKey(User)
     interest = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     
