f3626198f168e000ef52a268350e314a521b45e1
Vincent Zhang <iamkaruto@gmail.com>
2013-04-26 13:21:34 -0700

    Added firstname, lastname into userprofile models

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     django_user = models.ForeignKey(User)
+    firstname = models.CharField(max_length=200, default="Default Firstname")
+    lastname = models.CharField(max_length=200, default="Default Lastname")
     position = models.CharField(max_length=200)
     interest = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
