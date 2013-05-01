12d6c83e4d4cd498aa00da65cb4a9fa8692176b8
Joseph Tanigawa <jtanigawa@SONY-VPCSA2EGX.(none)>
2013-05-01 11:10:43 -0700

    Cleaned and commented accounts/views.py

--- event/views.py
+++ event/views.py
@@ -13,9 +13,9 @@ from taggit.models import Tag
 import time
 
 def index(request):
+    """
+    """
     template_var = {}
-    
-    # Retrieve s
     up = UserProfile.objects.filter(django_user=request.user)
     template_var["likes"] = Like.objects.filter(user=up[0])
     
@@ -25,10 +25,13 @@ def index(request):
         raise Http404
     output = ', '.join([e.title for e in event_list])    
     template_var["events"] = output	
-    return render_to_response("event/index.html", template_var, context_instance=RequestContext(request))
+    return render_to_response("event/index.html", template_var,
+                              context_instance=RequestContext(request))
 
 
 def single(request, pk):
+    """
+    """
     template_var = {}
     try:
         e = Event.objects.get(pk=int(pk))
@@ -44,9 +47,10 @@ def single(request, pk):
     
     template_var["allusers"] = UserProfile.objects.all()
     template_var["auth_users"] = User.objects.all()
-    current_django_user = UserProfile.objects.filter(django_user=request.user)[0];
+    current_django_user = UserProfile.objects.filter(django_user=request.user)[0]
         
-    return render_to_response("event/event_single.html", template_var, context_instance=RequestContext(request))
+    return render_to_response("event/event_single.html", template_var,
+                              context_instance=RequestContext(request))
     
 
 def archives(request):
@@ -117,7 +121,7 @@ def save_event(request):
     template_var = {}
     template_var["allusers"] = UserProfile.objects.all()
 
-    current_django_user = UserProfile.objects.filter(django_user=request.user)[0];
+    current_django_user = UserProfile.objects.filter(django_user=request.user)[0]
     
     if request.method=="POST":
         if request.user.is_authenticated():
@@ -183,7 +187,6 @@ def splitTags(user_input):
                 if(len(element) == 0): continue
                 if element not in tags:
                     tags.append(element)
-       
         
         for tag in tags:
                 print "(" + tag+ ")"
@@ -204,9 +207,7 @@ def msg_send(request):
             message.msg_from = UserProfile.objects.filter(django_user=request.user)[0]
             message.msg_to = UserProfile.objects.filter(id__exact=request.POST["msg_to_django_user_id"])[0]
             message.content = request.POST["content"]
-            message.save()
-
-            
+            message.save()    
 
         return HttpResponseRedirect("/events/msg/")
         
@@ -228,8 +229,6 @@ def search(request):
         tags = Tag.objects.all()
         template_var["tags"] = tags
 
-        
-
         query_tags = request.GET.getlist('query_tag')
         if len(query_tags) > 0:  # at least one checkbox selected
             advanced_search = True
@@ -241,9 +240,7 @@ def search(request):
                 query_tag_list.append(str(query_tag))
                 
             events_found_advanced = Event.objects.filter(tags__name__in=query_tag_list)
-            
-
-                
+               
         query = request.GET.get('query', '').strip('\t\n\r')
         if query == '' : #first came in/accidentily type space/..
             if advanced_search:
@@ -257,7 +254,6 @@ def search(request):
             else:
                 events_found_basic = Event.objects.filter(tags__name__in=[query])
 
-
         #finally
         if advanced_search:
             #take out duplicated result
@@ -268,8 +264,6 @@ def search(request):
         else:
             for event in events_found_basic:
                 events_found.append(event)
-
-        
 
         #sort result
         sorted_method = request.GET.get('sorted_method', 'desc') #default is desc
@@ -285,8 +279,8 @@ def search(request):
         request.session["sorted_method_session"] = sorted_method
         request.session["checkbox_session"] = checkbox_session
         request.session["query_session"] = query
-        
-
         return render_to_response("event/event_search_results.html",template_var,context_instance=RequestContext(request))
 
     return render_to_response("event/event_search_results.html",template_var,context_instance=RequestContext(request))
+    
+    
