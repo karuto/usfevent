e7ec91162f0f611a3102ba4c1e5b334ed32fb0c7
Vincent Zhang <iamkaruto@gmail.com>
2013-05-12 19:47:19 -0700

    Finished adding order form db spec

--- event/models.py
+++ event/models.py
@@ -24,8 +24,36 @@ class Event(models.Model):
         return self.title
         
 
-#class Order(models.Model):
+class Order(models.Model):
+    created_time = models.DateTimeField(auto_now=True)
+    client_name = models.CharField(max_length=100)
+    client_org = models.CharField(max_length=100)
+    client_phone = models.CharField(max_length=100)
+    client_email = models.EmailField(max_length=75)
+    client_is_sponsored = models.BooleanField(default=False)
+    project_name = models.CharField(max_length=100)
+    project_deadline = models.DateTimeField(default=datetime.now)
+    event_location = models.CharField(max_length=100)
+    event_time = models.DateTimeField(default=datetime.now)
+    design_text = models.TextField()
+    design_concept = models.TextField()
+    is_poster = models.BooleanField(default=False)
+    is_flyer = models.BooleanField(default=False)
+    is_handbill = models.BooleanField(default=False)
+    is_businesscard = models.BooleanField(default=False)
+    is_pamphlet = models.BooleanField(default=False)
+    is_invitation = models.BooleanField(default=False)
+    is_logo = models.BooleanField(default=False)
+    is_banner = models.BooleanField(default=False)
+    is_other = models.BooleanField(default=False)
+    project_other = models.CharField(max_length=100, blank=True)
 
+    def __unicode__(self):
+        return unicode("Project %s by %s" % (self.project_name, self.client_name))
+
+class OrderAdmin(admin.ModelAdmin):
+    display_fields = []
+admin.site.register(Order, OrderAdmin)    
 
 
 class Comment(models.Model):
@@ -36,8 +64,7 @@ class Comment(models.Model):
     
     def __unicode__(self):
         return unicode("%s: %s" % (self.event, self.content[:50]))
-        
-        
+       
 class CommentAdmin(admin.ModelAdmin):
     display_fields = []
 admin.site.register(Comment, CommentAdmin)    
